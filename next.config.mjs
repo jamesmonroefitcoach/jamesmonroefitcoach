db04f2becc0eb7fc7aaa13e768fa8f838a3d6f8a
Ryan Mecca <ryanmecca@Ryans-MacBook-Pro.local>
2026-05-11 14:46:00 -0500

    Exclude large devDependencies from build traces to fix Vercel deploy

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,19 @@
 const nextConfig = {
   reactStrictMode: true,
   eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: false }
+  typescript: { ignoreBuildErrors: false },
+  experimental: {
+    outputFileTracingExcludes: {
+      "*": [
+        "node_modules/@swc/**",
+        "node_modules/@esbuild/**",
+        "node_modules/webpack/**",
+        "node_modules/terser/**",
+        "node_modules/typescript/**",
+        "node_modules/eslint/**",
+      ],
+    },
+  },
 };
 
 export default nextConfig;
